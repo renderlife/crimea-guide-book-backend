8fa420ccd8faa74c56e3d8e40e7d35db9bfa59de
Renderlife <egnko@mail.ru>
2019-07-09 22:10:49 +0600

    Create new table db

---
+++ database/migrations/2019_07_09_205823_create_cities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code', 20);
+            $table->string('name', 100);
+            $table->string('coordinate', 50);
+            $table->integer('zoom');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
